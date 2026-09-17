5c2a5d1f07bbfdf0206ce807aceeb4234ef74828
Tarun <132468329+tkmourya@users.noreply.github.com>
2026-09-17 22:56:36 +0530

    white line between status bar and app page fix

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,39 @@ export default defineConfig(() => {
       tailwindcss(),
       VitePWA({
         registerType: 'prompt',
+        workbox: {
+          globPatterns: ['**/*.{js,css,html,ico,png,svg,jpg,jpeg}'],
+          runtimeCaching: [
+            {
+              urlPattern: /^https:\/\/api\.themoviedb\.org\/3\/.*/i,
+              handler: 'NetworkFirst',
+              options: {
+                cacheName: 'tmdb-api-cache',
+                expiration: {
+                  maxEntries: 100,
+                  maxAgeSeconds: 60 * 60 * 24 * 7 // 1 week
+                },
+                cacheableResponse: {
+                  statuses: [0, 200]
+                }
+              }
+            },
+            {
+              urlPattern: /^https:\/\/image\.tmdb\.org\/t\/p\/.*/i,
+              handler: 'CacheFirst',
+              options: {
+                cacheName: 'tmdb-image-cache',
+                expiration: {
+                  maxEntries: 300,
+                  maxAgeSeconds: 60 * 60 * 24 * 30 // 30 days
+                },
+                cacheableResponse: {
+                  statuses: [0, 200]
+                }
+              }
+            }
+          ]
+        },
         manifest: {
           name: 'Parlaxio',
           short_name: 'Parlaxio',
